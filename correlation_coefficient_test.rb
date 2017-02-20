1111e6c74c69738c6a5aed42184c7e3fe8a9c131
Bartek <bartekm93@gmail.com>
2017-02-20 15:17:10 +0100

    ci test 2

--- correlation_coefficient_test.rb
+++ correlation_coefficient_test.rb
@@ -3,10 +3,6 @@ require_relative 'critics'
 require 'minitest/autorun'
 require 'minitest/ci'
 
-if ENV["CIRCLECI"]
-  Minitest::Ci.report_dir = "#{ENV["CIRCLE_TEST_REPORTS"]}/reports"
-end
-
 class CorrelationCoefficientTestSuit < MiniTest::Unit::TestCase
     NONEXISTING_USER_1 = "Krzesło"
     NONEXISTING_USER_2 = "Lampa"
