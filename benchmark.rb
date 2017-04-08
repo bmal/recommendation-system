01b8c884ec54b00d6c8e406d494254f4352ede84
Bartek <bartekm93@gmail.com>
2017-04-08 22:13:24 +0200

    Benchmark added

--- benchmark.rb
+++ benchmark.rb
@@ -1,10 +1,62 @@
-require_relative 'recommendation_system_factory'
-require_relative 'correlation_coefficient_factory'
-require_relative 'movie_lens_100k_reader'
-require_relative 'test/test_helper'
-require_relative 'cross_validation'
+class Benchmark
+    def initialize(folds, seed: 152)
+        @folds = folds
+        @seed = seed
+    end
 
-prefs = TestHelper::CRITICS #MovieLens100kReader.new.get_prefs
-CrossValidation.new(prefs, 2).each {|chunk| puts "testowy: #{chunk[:testing_set]}\ntreningowy: #{chunk[:training_set]}" }
-similarity = CorrelationCoefficientFactory.new(prefs).createTanimotoSimilarity
-p RecommendationSystemFactory.new(prefs, similarity).createCollaborativeFilteringSystem.calculate_recommendations('Donald Trump')
+    def perform(removal_factor: 0.3, &recommendation_system_creator)
+        results = []
+
+        @folds.each do |fold|
+            fold_results = {}
+            data_set, filtered_objects = prepare_data_set(fold, removal_factor)
+
+            fold_results[:system_generation_time], recommendation_system  = count_time_and_perform { recommendation_system_creator.call(data_set) }
+
+            users = fold[:testing_set].keys
+            users_results = users.map do |user|
+                time_of_recommendation_generation, recommendations_list = count_time_and_perform { recommendation_system.calculate_recommendations(user) }
+                { user => {
+                    time_of_recommendation_generation: time_of_recommendation_generation,
+                    recommendations: recommendations_list,
+                    filtered_rated_objects: filtered_objects[user] }}
+            end
+
+            fold_results[:calculation_results] = users_results
+            results << fold_results
+        end
+
+        results
+    end
+
+    private
+    def count_time_and_perform(&operation)
+        starting_time = Time.now
+        result = operation.call
+        ending_time = Time.now 
+
+        [ending_time - starting_time, result]
+    end
+
+    def prepare_data_set(fold, removal_factor)
+        filtered_testing_set, removed_objects = filter_testing_set(fold[:testing_set], removal_factor)
+        data_set = fold[:training_set].merge(filtered_testing_set)
+
+        [data_set, removed_objects]
+    end
+
+    def filter_testing_set(original_testing_set, removal_factor)
+        new_training_set = {}
+        removed_elements = {}
+
+        original_testing_set.each do |user, ratings|
+            shuffled_ratings = ratings.to_a.shuffle(random: Random.new(@seed))
+            num_of_elements_to_remove = shuffled_ratings.size * removal_factor
+
+            new_training_set[user] = shuffled_ratings[num_of_elements_to_remove...shuffled_ratings.size].to_h
+            removed_elements[user] = shuffled_ratings[0...num_of_elements_to_remove].to_h.keys
+        end
+
+        [new_training_set, removed_elements]
+    end
+end
